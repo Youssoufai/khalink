3e3779b1fa230c61f2518202666ef352e5b10766
Yusuf Muhammad <yusuf.muhammad@eykegai.com>
2026-09-21 08:41:34 +0100

    Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
   title: "Khalil Nur Khalil | Economist & Investment Strategist",
   description:
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
